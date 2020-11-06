492cb924b53f6753f2fc908250884a246b81bc00
theochampion <contact.theochampion@gmail.com>
2020-11-06 11:18:50 +0100

    :construction: Added user key entry in log

--- api/log.ts
+++ api/log.ts
@@ -9,9 +9,12 @@ export default async (req: NowRequest, res: NowResponse) => {
   const {firstname, lastname, birthday, pob, latitude, longitude} = JSON.parse(
     req.body
   );
+
+  const userKey = `${firstname.toLowerCase()}_${lastname.toLowerCase()}`;
   await serverClient.query(
     q.Create(q.Collection('users'), {
       data: {
+        key: userKey,
         firstname,
         lastname,
         birthday,
@@ -22,6 +25,7 @@ export default async (req: NowRequest, res: NowResponse) => {
     })
   );
   res.send({
+    key: userKey,
     firstname,
     lastname,
     birthday,
