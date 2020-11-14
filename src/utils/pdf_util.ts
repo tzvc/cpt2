7a31d66e3cf1a724478a0b48bad09189764c91d4
theochampion <contact.theochampion@gmail.com>
2020-11-14 23:49:27 +0100

    :sparkles: Updated to new version of base PDF

--- src/utils/pdf_util.ts
+++ src/utils/pdf_util.ts
@@ -1,17 +1,17 @@
-import {PDFDocument, StandardFonts} from 'pdf-lib';
+import {PDFDocument, rgb, StandardFonts} from 'pdf-lib';
 import baseCertificate from './baseCertificate.pdf';
 import {generateQR} from './qrcode_util';
 
 const ys = new Map([
-  ['travail', 578],
-  ['achats', 533],
-  ['sante', 477],
-  ['famille', 435],
-  ['handicap', 396],
-  ['sport_animaux', 356],
-  ['convocation', 295],
-  ['missions', 255],
-  ['enfants', 211],
+  ['travail', 488],
+  ['achats', 417],
+  ['sante', 347],
+  ['famille', 325],
+  ['handicap', 291],
+  ['sport_animaux', 269],
+  ['convocation', 199],
+  ['missions', 178],
+  ['enfants', 157],
 ]);
 
 export type ATInfos = {
@@ -47,7 +47,8 @@ export async function generatePdf(infos: ATInfos, reason: string) {
     `Adresse: ${address} ${zipcode} ${city}`,
     `Sortie: ${datesortie} a ${heuresortie}`,
     `Motifs: ${reason}`,
-  ].join(';\n ');
+    '',
+  ].join(';\n');
 
   const existingPdfBytes = await fetch(baseCertificate).then((res) =>
     res.arrayBuffer()
@@ -78,12 +79,12 @@ export async function generatePdf(infos: ATInfos, reason: string) {
     page1.drawText(text, {x, y, size, font});
   };
 
-  drawText(`${firstname} ${lastname}`, 119, 696);
-  drawText(birthday, 119, 674);
-  drawText(placeofbirth, 297, 674);
-  drawText(`${address} ${zipcode} ${city}`, 133, 652);
+  drawText(`${firstname} ${lastname}`, 107, 657);
+  drawText(birthday, 107, 627);
+  drawText(placeofbirth, 240, 627);
+  drawText(`${address} ${zipcode} ${city}`, 124, 596);
 
-  drawText('x', 78, ys.get(reason)!, 18);
+  drawText('x', 59, ys.get(reason)!, 12);
 
   let locationSize = getIdealFontSize(font, infos.city, 83, 7, 11);
 
@@ -95,11 +96,22 @@ export async function generatePdf(infos: ATInfos, reason: string) {
     locationSize = 7;
   }
 
-  drawText(infos.city, 105, 177, locationSize);
-  drawText(`${infos.datesortie}`, 91, 153, 11);
-  drawText(`${infos.heuresortie}`, 264, 153, 11);
+  drawText(infos.city, 93, 122, locationSize);
+  drawText(`${infos.datesortie}`, 76, 92, 11);
+  drawText(`${infos.heuresortie}`, 246, 92, 11);
 
+  const qrTitle1 = 'QR-code contenant les informations ';
+  const qrTitle2 = 'de votre attestation numérique';
   const generatedQR = await generateQR(data);
+
+  page1.drawText(qrTitle1 + '\n' + qrTitle2, {
+    x: 415,
+    y: 135,
+    size: 9,
+    font,
+    lineHeight: 10,
+    color: rgb(1, 1, 1),
+  });
 
   const qrImage = await pdfDoc.embedPng(generatedQR);
 
@@ -112,9 +124,17 @@ export async function generatePdf(infos: ATInfos, reason: string) {
 
   pdfDoc.addPage();
   const page2 = pdfDoc.getPages()[1];
+  page2.drawText(qrTitle1 + qrTitle2, {
+    x: 50,
+    y: page2.getHeight() - 70,
+    size: 11,
+    font,
+    color: rgb(1, 1, 1),
+  });
+
   page2.drawImage(qrImage, {
     x: 50,
-    y: page2.getHeight() - 350,
+    y: page2.getHeight() - 390,
     width: 300,
     height: 300,
   });
